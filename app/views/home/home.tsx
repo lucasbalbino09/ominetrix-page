69ef22d93c91aaf4e7541fa2bd82fce04b45ef04
Lu_Balbino <98754988+lucasbalbino09@users.noreply.github.com>
2024-11-22 15:03:52 -0300

    Botoes Ajustados.

--- app/views/home/home.tsx
+++ app/views/home/home.tsx
@@ -30,7 +30,7 @@ export default function Teste() {
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-        {["Cadastrar", "Starred", "Send email", "Drafts"].map((text, index) => (
+        {["Cadastrar", "Lista Completa"].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
@@ -40,20 +40,7 @@ export default function Teste() {
             </ListItemButton>
           </ListItem>
         ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      </List>      
     </Box>
   );
   return (
