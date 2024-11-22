b2ff4a0021e39b110cc0743eda54b69d22cbe0d2
Lu_Balbino <98754988+lucasbalbino09@users.noreply.github.com>
2024-11-22 15:40:22 -0300

    adicionando rota no menu lateral

--- app/views/home/home.tsx
+++ app/views/home/home.tsx
@@ -8,8 +8,10 @@ import Typography from "@mui/material/Typography";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import Button from "@mui/material/Button";
 import List from "@mui/material/List";
+import Link from "next/link";
 import MenuIcon from "@mui/icons-material/Menu";
 import MailIcon from "@mui/icons-material/Mail";
+
 import {
   ListItem,
   ListItemButton,
@@ -25,16 +27,27 @@ export default function Teste() {
     setOpen(newOpen);
   };
 
+  const menuLateral = [
+    {
+      text: "Cadastro",
+      rota: "/cadastrar"      
+    },
+    {
+      text: "Lista Completa",
+      rota: "/tabela"      
+    }
+  ]
+
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-        {["Cadastrar", "Lista Completa"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
+        {menuLateral.map((item, index) => (
+          <ListItem key={item.text} disablePadding>
+            <ListItemButton component={Link} href={item.rota}>
               <ListItemIcon>
                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={item.text} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -46,13 +59,13 @@ export default function Teste() {
       <AppBar position="static">
         <Toolbar>
           <Button onClick={toggleDrawer(true)} sx={{ mr: 2 }}>
-            <MenuIcon></MenuIcon>
+            <MenuIcon color="secondary"></MenuIcon>
           </Button>
           <Drawer open={open} onClose={toggleDrawer(false)}>
             {DrawerList}
           </Drawer>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Level-up
+            OmineTrix
           </Typography>
             <Button color="inherit" disabled>
               Login
